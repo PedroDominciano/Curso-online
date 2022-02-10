13c3b82bfedf72318a391ec61db518076d8ac7fa
Pedro Dominciano <pedrocb82@gmail.com>
2022-02-10 14:01:24 -0300

    Primeira classe de teste

---
+++ test/CursoOnline.DominioTest/MeuPrimeiroTeste.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest
+{
+    public class MeuPrimeiroTeste
+    {
+        [Fact(DisplayName = "Variavel1MustBeEqualToVariavel2")]
+        public void Variavel1MustBeEqualToVariavel2()
+        {
+            //Arrange
+            var variavel1 = 1;
+            var variavel2 = 1;
+
+            //Action
+            variavel1 = variavel2;
+
+            //Assert
+            Assert.Equal(variavel1, variavel2);
+        }
+    }
+}
