1ebaaf3fb4694a5d6e2fd86e772850f884dfc1f0
Pedro Dominciano <pedrocb82@gmail.com>
2022-02-11 11:02:15 -0300

    Classe CursoTest criada

---
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTest
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            //Arranje
+            const string nome = "Informática básica";
+            const double cargaHoraria = 80;
+            const string publicoAlvo = "Estudante";
+            const double valor = 950;
+
+            //Action
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            //Assert
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+
+    }
+}
